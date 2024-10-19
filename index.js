33ba3c3d7175226e48dd4015b2b74b563d4d643d
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-19 12:20:42 +0700

    projectpertemuan3

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mobilRoute from "./routes/Mobil.js";
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.json());
+
+app.use("/mobil", mobilRoute);
+app.get("/", (req, res) => {
+    console.log(['GET ROUTE']);
+    res.send("Selamat Pagi");
+});
+
+
+app.use(bodyParser.json());
+app.listen(PORT, () =>
+    console.log(`Server berjalan di port : http://localhost:${PORT}`)
+);
+
+
---
+++ routes/Mobil.js
@@ -0,0 +1,21 @@
+import express, { Router } from "express";
+const router = express.Router();
+const mobil = [
+    {
+        merk: "Toyota",
+        model: "Avanza",
+        warna: "Hitam",
+        tahun: 2021,
+    },
+    {
+        merk: "Porsche",
+        model: "sport",
+        warna: "Kuning",
+        tahun: 2024,
+    },
+];
+
+router.get("/", (req, res) => {
+    res.send(mobil);
+});
+export default router;
